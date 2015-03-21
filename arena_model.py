a435d20412a9befa6063438d1e3dbf607803effe
Shachar Raindel <shacharr@gmail.com>
2015-03-21 18:04:52 +0200

    Move the roomba step logic to the roomba model, leave only the glue logic in the controller

--- arena_model.py
+++ arena_model.py
@@ -1,5 +1,6 @@
 import math
 import pygame
+import random
 
 from helper_functions import *
 
@@ -14,7 +15,11 @@ class Point(object):
     def dot(self,other):
         return self.x*other.x + self.y*other.y
 
+MODE_TIME_LIMIT = [1000,5000]
+
 class RoombaModel(object):
+    TURN_SIZE_ON_WALL_FOLLOW = math.pi/180.
+    MAX_TURN_STEPS = 360
     def __init__(self, location, size, cleaning_head_size, direction, speed, room):
         self.loc = location
         self.direction = direction
@@ -22,19 +27,21 @@ class RoombaModel(object):
         self.size = size
         self.room = room
         self.cleaning_head_size = cleaning_head_size
+        self.in_random_direction_mode = False
+        self.time_in_mode = 0
 
-    def calc_step_next_loc(self):
+    def calc_move_next_loc(self):
         x,y = self.loc
         step_x = -self.speed * math.sin(self.direction)
         step_y = self.speed * math.cos(self.direction)
         return (x+step_x, y+step_y)
 
-    def check_step(self):
-        new_loc = self.calc_step_next_loc()
+    def check_move(self):
+        new_loc = self.calc_move_next_loc()
         return self.room.is_coliding(new_loc,self.size)
 
-    def step(self):
-        new_loc = self.calc_step_next_loc()
+    def move(self):
+        new_loc = self.calc_move_next_loc()
         # Assumes speed is slow enough to prevent quantom tunneling of the roomba...
         if not self.room.is_coliding(new_loc,self.size):
             mid_point = [(x+y)/2. for x,y in zip(new_loc,self.loc)]
@@ -60,6 +67,27 @@ class RoombaModel(object):
         self.room.clean_triangle(cleaned_triangle_2)
 
         self.direction += relative_direction
+
+    def step(self):
+        if not self.in_random_direction_mode:
+            for i in range(self.MAX_TURN_STEPS):
+                self.turn(-self.TURN_SIZE_ON_WALL_FOLLOW)
+                if self.check_move():
+                    break
+            self.turn(self.TURN_SIZE_ON_WALL_FOLLOW)
+        collided = self.move()
+        self.time_in_mode += 1
+        if collided:
+            if self.in_random_direction_mode:
+                self.turn(random.randint(0,360)*math.pi/180.)
+            else:
+                while self.check_move():
+                    self.turn(self.TURN_SIZE_ON_WALL_FOLLOW)
+            if self.time_in_mode > MODE_TIME_LIMIT[self.in_random_direction_mode]:
+                self.in_random_direction_mode = not self.in_random_direction_mode
+                self.time_in_mode = 0
+                print "Switched to mode",self.in_random_direction_mode
+
 
     def get_draw_info(self):
         return ([int(x) for x in self.loc],self.direction)
--- controller.py
+++ controller.py
@@ -7,7 +7,6 @@ import arena_model
 import arena_view
 
 
-MODE_TIME_LIMIT = [1000,5000]
 
 #ROOM_POLYGON = [(0,0),(640,0),(640,480),(0,480)]
 ROOM_POLYGON = [(0,0),(640,0),(640,480),(320,480),(320,240),(0,240)]
@@ -25,8 +24,6 @@ def main():
     room_model = arena_model.RoomModel(ROOM_POLYGON)
     roomba_model = arena_model.RoombaModel((100,100), ROOMBA_SIZE, 1.9, 0, 3, room_model)
     done = False
-    in_random_direction_mode = False
-    time_in_mode = 0
     while True:
         view.clear_screen(room_model.state)
         clock.tick(60)
@@ -37,25 +34,8 @@ def main():
                 done=True
         if done:
             break
-        if not in_random_direction_mode:
-            for i in range(MAX_TURN_STEPS):
-                roomba_model.turn(-TURN_SIZE_ON_WALL_FOLLOW)
-                if roomba_model.check_step():
-                    break
-            roomba_model.turn(TURN_SIZE_ON_WALL_FOLLOW)
-        collided = roomba_model.step()
+        roomba_model.step()
         view.draw_roomba(*roomba_model.get_draw_info())
-        time_in_mode += 1
-        if collided:
-            if in_random_direction_mode:
-                roomba_model.turn(random.randint(0,360)*math.pi/180.)
-            else:
-                while roomba_model.check_step():
-                    roomba_model.turn(TURN_SIZE_ON_WALL_FOLLOW)
-            if time_in_mode > MODE_TIME_LIMIT[in_random_direction_mode]:
-                in_random_direction_mode = not in_random_direction_mode
-                time_in_mode = 0
-                print "Switched to mode",in_random_direction_mode
     
 if __name__ == "__main__":
     main()
